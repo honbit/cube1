9a0522474217623544187debd8297e32b3c9d3c3
honbit.0 <honbit.0@gmail.com>
2023-11-27 10:18:12 +0000

    Batch update files

--- model/globals.py
+++ model/globals.py
@@ -10,3 +10,26 @@ template = TemplateContext()
 @template.function('is_accessible_by_team')
 def is_accessible_by_team(team: str, ctx: dict) -> bool:
   return team == ctx['securityContext'].setdefault('team', 'default')
+
+
+
+from cube_dbt import Dbt
+
+template = TemplateContext()
+
+manifest_url = 'https://cube-dbt-integration.s3.amazonaws.com/manifest.json'
+
+dbt = Dbt.from_url(manifest_url).filter(paths=['marts/'])
+
+for model in dbt.models:
+  print(model.name)
+
+@template.function('dbt_models')
+def dbt_models():
+  return dbt.models
+
+@template.function('dbt_model')
+def dbt_model(name):
+  return dbt.model(name)
+
+  
